4f2e40fd1399e6edd4cef2343d649c6ab261bd24
Pavel-m19 <pashok-ok@yandex.ru>
2022-03-09 17:59:45 +0300

    задиача 4 модуля 1

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,25 +1,8 @@
 function makeFriendsList(friends) {
-  let table = document.createElement('ul');
-  
-  for (val of friends) {
-    let string = document.createElement('li');
-    string.innerHTML(`${val.firstName} ${val.lastName}`);
-    table.appendChild(string);
-  }
-  return table;
+  let ul = document.createElement('ul');
+    
+  ul.innerHTML = friends.map(val =>
+    `<li>${val.firstName} ${val.lastName}</li>`).join('') ;
+  return ul;
 }
 
-console.log(friends([
-  {
-    firstName: 'Artsiom',
-    lastName: 'Mezin'
-  },
-  {
-    firstName: 'Ilia',
-    lastName: 'Kantor'
-  },
-  {
-    firstName: 'Christopher',
-    lastName: 'Michael'
-  }
-]))
