f6f5d0f6c11f9816da96784e750d7690fee253cf
Pavel-m19 <pashok-ok@yandex.ru>
2022-03-25 12:17:29 +0300

    задиачи 4 модуля, тесты пройдены

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,30 @@
 function highlight(table) {
-  // ваш код...
+   
+  for (i = 1; i < table.childNodes[3].childNodes.length;) {
+    let row = table.childNodes[3].childNodes[i];
+        
+    if (row.childNodes[7].getAttribute('data-available') === 'true') {
+      row.classList.add("available");
+    } else if (row.childNodes[7].getAttribute('data-available') === 'false') {
+      row.classList.add("unavailable") ;     
+    } else {
+      
+      row.hidden = true;      
+    }
+
+    if (row.childNodes[5].innerHTML === "m") {
+      row.className += " male";
+    } else if (row.childNodes[5].innerHTML === "f") {
+      row.className += " female";
+    }
+
+  
+    
+    
+    if (row.childNodes[3].innerHTML < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+
+    i += 2;
+  }
 }
