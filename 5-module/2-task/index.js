4554a7522d47a702303d40f0838e742fafe0392a
Pavel-m19 <pashok-ok@yandex.ru>
2022-03-29 13:10:49 +0300

    задиачи 5 модуля, тесты пройдены

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,7 @@
 function toggleText() {
-  // ваш код...
+  text = document.getElementById('text');
+  btn = document.querySelector('.toggle-text-button');  
+  btn.onclick = function() {
+    text.hidden === true ? text.hidden = false: text.hidden = true;
+  };
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,38 @@
 function initCarousel() {
-  // ваш код...
+  let counter = 0;
+  let rightArrow = document.querySelector('.carousel__arrow_right');
+  let leftArrow = document.querySelector('.carousel__arrow_left');
+  let image = document.querySelector('.carousel__inner');
+  let Xoffset = 0;
+  let maxCount = document.querySelectorAll('.carousel__slide').length;
+  console.log('max', maxCount);
+
+  //функция, которая считает слайды и гасит стрелочки
+  function arrowSwitcher() {    
+    if (counter === 0) {
+      leftArrow.style.display = 'none';
+    } else {leftArrow.style.display = '';}
+    if (counter === maxCount - 1) {
+      rightArrow.style.display = 'none';    
+    } else {rightArrow.style.display = '';}   
+    //console.log(counter);
+  }
+  arrowSwitcher(); //инициализация отображения стрелочек
+  
+  rightArrow.onclick = function() {
+    counter += 1;
+    let offset = image.offsetWidth;
+    Xoffset -= offset;    
+    image.style.transform = `translateX(${Xoffset}px)`;
+    arrowSwitcher(counter);     
+  };
+
+  leftArrow.onclick = function() {
+    counter -= 1;
+    let offset = image.offsetWidth;
+    Xoffset += offset;
+    image.style.transform = `translateX(${Xoffset}px)`;
+    arrowSwitcher(counter);       
+  };
+
 }
