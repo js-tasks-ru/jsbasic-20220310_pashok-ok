787c8c9397fbffa99fe2e0526ed33fb74f1aca50
Pavel-m19 <pashok-ok@yandex.ru>
2022-04-12 09:13:23 +0300

    проблема с тестами задачи 7.4

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -3,5 +3,77 @@ import createElement from '../../assets/lib/create-element.js';
 export default class RibbonMenu {
   constructor(categories) {
     this.categories = categories;
+    this.elem = this.render();
+  }
+
+  render() {
+    const ribbonsItems = this.categories.map(item => {
+      return `<a href="#" class="ribbon__item" data-id="${item.id}">${item.name}</a>`;      
+    }).join('');
+
+    const ribbonMenu = createElement(`<div class="ribbon">    
+    <button class="ribbon__arrow ribbon__arrow_left">
+      <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+    </button>
+
+    <nav class="ribbon__inner">
+    ${ribbonsItems}
+    </nav>
+    
+    <button class="ribbon__arrow ribbon__arrow_right ribbon__arrow_visible">
+      <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+    </button>
+  </div>`);   
+
+    ribbonMenu.querySelectorAll('.ribbon__item')[0].classList.add('ribbon__item_active');    
+    
+    let ribbonInner = ribbonMenu.querySelector('.ribbon__inner');     
+    let scrLeft = ribbonInner.scrollLeft;                 
+    let scrRight; 
+
+    let rightArrow = ribbonMenu.querySelector('.ribbon__arrow_right');
+    let leftArrow = ribbonMenu.querySelector('.ribbon__arrow_left');          
+    
+    //функция, которая считает слайды и гасит стрелочки
+    ribbonInner.addEventListener('scroll', event => {      
+      
+      let clientWidth = ribbonInner.clientWidth;       
+      let scrWidth = ribbonInner.scrollWidth; 
+      
+      scrLeft = ribbonInner.scrollLeft;
+      scrRight = scrWidth - scrLeft - clientWidth;
+    
+      if (scrLeft === 0) {        
+        leftArrow.classList.remove('ribbon__arrow_visible');
+      } else {leftArrow.classList.add('ribbon__arrow_visible');}
+      if (scrRight < 1) {
+        rightArrow.classList.remove('ribbon__arrow_visible');   
+      } else {rightArrow.classList.add('ribbon__arrow_visible');}       
+    }); 
+
+    rightArrow.onclick = function() {               
+      ribbonInner.scrollBy(350, 0);                   
+    };
+
+    leftArrow.onclick = function() {           
+      ribbonInner.scrollBy(-350, 0);                  
+    };   
+    
+    ribbonMenu.addEventListener('click', event =>{
+      
+      if (event.target.tagName === 'A') {  
+        event.preventDefault();
+        ribbonMenu.querySelector('.ribbon__item_active').classList.remove('ribbon__item_active');
+        event.target.classList.add('ribbon__item_active');  
+        const category = event.target.dataset;                            
+        const pressEvent = new CustomEvent('ribbon-select', { // имя события должно быть именно 'ribbon-select'
+          detail: category.id, // уникальный идентификатора категории из её объекта
+          bubbles: true // это событие всплывает - это понадобится в дальнейшем
+        });
+        event.target.dispatchEvent(pressEvent);      
+      }           
+    });
+
+    return ribbonMenu;  
   }
 }
--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,62 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+    this.elem = this.render(steps, value);
+    this.steps = steps;
+  }
+
+  render(steps, value) {
+
+    let spans = '<span></span>'.repeat(steps);
+    let slider = createElement(
+      `<div class="slider">
+        <!--Ползунок слайдера с активным значением-->
+        <div class="slider__thumb">
+          <span class="slider__value">${value}</span>
+        </div>
+
+        <!--Полоска слайдера-->
+        <div class="slider__progress"></div>
+
+        <!-- Шаги слайдера (вертикальные чёрточки) -->
+        <div class="slider__steps">         
+          ${spans}          
+        </div>
+      </div>`
+    );
+    
+    const spansList = slider.querySelector('.slider__steps').querySelectorAll('span');    
+    spansList[value].classList.add("slider__step-active");
+
+    const sliderProgress = slider.querySelector('.slider__progress');
+    sliderProgress.style.width = `${(value / (steps - 1))*100}%`
+    
+    slider.addEventListener('click', event =>{ 
+      
+      let sliderWidth = slider.getBoundingClientRect().right - slider.getBoundingClientRect().x;
+      let clickX = event.clientX - slider.getBoundingClientRect().x; 
+
+      value = Math.round(clickX / (sliderWidth / (steps - 1)));
+      console.log(value);
+
+      slider.querySelector('.slider__step-active').classList = '';
+      spansList[value].classList.add("slider__step-active");
+      slider.querySelector(".slider__value").innerHTML = value;
+      document.querySelector('.slider__thumb').style.left = `${value / (steps - 1) * 100}%`;
+      sliderProgress.style.width = `${(value / (steps - 1)) * 100}%`;
+
+      this.value = value;
+
+      const pressEvent = new CustomEvent('slider-change', {
+        detail: this.value, // значение 0, 1, 2, 3, 4
+        bubbles: true // событие всплывает - это понадобится в дальнейшем
+      });
+      event.target.dispatchEvent(pressEvent);
+      
+    });
+
+    return slider;
+
   }
 }
